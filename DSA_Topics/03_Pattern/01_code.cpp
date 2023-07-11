ce2c24dfbf282196d8e44e0bb1c598d07c4adc58
Sahil Sharma <sahilwep@gmail.com>
2023-07-11 14:32:51 +0530

    Patterns

--- DSA_Topics/03_Pattern/01_code.cpp
+++ DSA_Topics/03_Pattern/01_code.cpp
@@ -87,6 +87,22 @@
               * * *         
                 *
     
+    // pattern 9
+
+                * 
+              * * * 
+            * * * * * 
+          * * * * * * *  
+        * * * * * * * * *
+        * * * * * * * * *         
+          * * * * * * *     
+            * * * * *              
+              * * *         
+                *
+    
+    // pattern 10
+
+    
 */
 
 #include <iostream>
@@ -258,6 +274,47 @@ void pattern_8(int n){
     }
 }
 
+void pattern_9(int n){
+/*
+    // for this pattern combine pattern_(7 & 8)
+
+                * 
+              * * * 
+            * * * * * 
+          * * * * * * *  
+        * * * * * * * * *   
+        * * * * * * * * *      
+          * * * * * * *    
+            * * * * *                  
+              * * *        
+                *          
+*/
+
+    for(int i = 1 ; i<=n; i++){
+        for(int j = 1; j <= n-i; j++){
+            cout << "  ";
+        }
+        for(int j = 1; j<=2*i-1; j++){
+            cout << "* ";
+        }
+        for(int j = 1; j <= n-i; j++){
+            cout << "  ";
+        }
+        cout << endl;
+    }
+    for(int i=1; i <= n;i++){
+        for(int j=1;j <= i-1 ;j++){
+            cout << "  ";
+        }
+        for(int j =1; j<= 2*n-(2*i-1);j++){
+            cout << "* ";
+        }
+        for(int j=1;j <= i-1 ;j++){
+            cout << "  ";
+        }
+        cout << endl;
+    }
+}
 
 int main(void){ 
 
@@ -275,6 +332,7 @@ int main(void){
         pattern_6_3(n);
         pattern_7(n);
         pattern_8(n);
+        pattern_9(n);
     }
 
     return 0;
