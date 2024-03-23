9c73b7acc9f3f8de94ef968acf86398eb1540223
Sahil Sharma <sahilwep@gmail.com>
2024-03-23 13:45:21 +0530

    linked list: Application

---
+++ GFG_Complete_A_Z/12_Linked_List/13_code.cpp
@@ -0,0 +1,29 @@
+/*
+// Application of Linked list: 
+    * Linked list is growable dynamically allocated data structure.
+
+        [value|address]--->[value|address]--->[value|address]--->NULL
+            ^
+            |
+            |
+        First Node
+
+    // Example: 
+
+                [10|1004]--->[20|1001]--->[30|NULL]--->NULL
+     Address:     892           1004                1001        
+
+
+    // Operations of Linked List:
+        * Insertion
+            * Insertion beg
+            * Insertion end
+            * Insertion at any nth position
+        * Deletion
+            * Deletion beg
+            * Deletion end
+            * Deletion at any nth position 
+        * Traversal : 
+            * Looping
+            * Recursive
+*/
