148d2a56fe9c7465b11deb4a8435290248772d14
Sahil Sharma <sahilwep@gmail.com>
2024-03-28 10:23:39 +0530

    Linked list: Circular linked list

---
+++ GFG_Complete_A_Z/12_Linked_List/21_code.cpp
@@ -0,0 +1,11 @@
+/*
+// Circular linked list Advantages & disadvantages: 
+
+    * As the last node of the circular linked list is connected with head, the insertion & deletion at the beg & end become the O(1)
+
+    * insert beg: O(1)
+    * insert end: O(1)
+    * delete beg: O(1)
+    * delete end: O(1)
+
+*/
