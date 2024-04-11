02c3cda9a9e84fee6ef35707aea7cebec3d7df42
Sahil Sharma <sahilwep@gmail.com>
2024-04-11 16:43:08 +0530

    Bitmagic: Bitwise 1

--- GFG_Complete_A_Z/04_Bit_Magic/01_code.cpp
+++ GFG_Complete_A_Z/04_Bit_Magic/01_code.cpp
@@ -1,14 +1,86 @@
+/*
+// Bitwise Operators: 
+      * These operators are the logic gates operators AND, OR, XOR...
+
+    Bitwise: OR  |
+
+          A | B  =>  C
+        --------------
+          0 | 0  =>  0       
+          0 | 1  =>  1       
+          1 | 0  =>  1       
+          1 | 1  =>  1       
+
+                * Example: 
+                              3 = 00000000...0011
+                              6 = 00000000...0110  (|) OR
+                            -------------------------
+                                  00000000...0111      => 7
+
+  * Bitwise: AND  &
+
+          A | B  =>  C
+        --------------
+          0 | 0  =>  0       
+          0 | 1  =>  0       
+          1 | 0  =>  0       
+          1 | 1  =>  1       
+          
+                * Example: 
+                              3 = 00000000...0011
+                              6 = 00000000...0110  (&) AND
+                            -------------------------
+                                  00000000...0010      => 2
+
+  * Bitwise: XOR  ^  : Same false, different true
+
+          A | B  =>  C
+        --------------
+          0 | 0  =>  0       
+          0 | 1  =>  1       
+          1 | 0  =>  1       
+          1 | 1  =>  0       
+
+                * Example: 
+                              3 = 00000000...0011
+                              6 = 00000000...0110  (^) OR
+                            -------------------------
+                                  00000000...0101      => 5
+
+*/ 
+
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
+void bitwiseOR(){
+    int x = 3;
+    int y = 6;
+
+    cout << "OR: " <<  (x | y) << endl;
+}
+
+void bitwiseAND(){
+    int x = 3;
+    int y = 6;
+
+    cout << "AND: " << (x & y) << endl;
+}
+
+void bitwiseXOR(){
+    int x = 3;
+    int y = 6;
+
+    cout << "XOR: " << (x ^ y) << endl;
+}
 
 
 int main(void){
-  long long n, m, a;
-  cin >> n >> m >> a;
-  n = n/a + (n%a != 0);
-  m = m/a + (m%a != 0);
-  cout << n*m << endl;
-
+    
+    bitwiseOR();
+    bitwiseAND();
+    bitwiseXOR();
+    
+    
     return 0;
 }
