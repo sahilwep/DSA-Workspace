7fc0d61712c8aab9c29858512aaf4a517c36d3ca
Sahil Sharma <sahilwep@gmail.com>
2024-04-24 13:52:30 +0530

    Codeforces: A. Game with Integers

--- Codeforces/800/A_Game_with_Integers.cpp
+++ Codeforces/800/A_Game_with_Integers.cpp
@@ -2,7 +2,38 @@
 //  A. Game with Integers               https://codeforces.com/problemset/problem/1899/A
 
 
+// Observation: 
+    * As the game says, player have to increase or decrease 1 to the number, in order to win...
+    * both player are playing optimally, in order to win...
+    * if Vanya wins we have to print first : else second for Vova
+    * Note : if 10 moves has passed Vanya has not won, then vova wins..
 
+    * IF we observe carefully: 
+        n % 3 = 0 : 3, 6, 9, 12, 15, 18, 21, 24....
+        when remainder is 0 : these are the numbers..
+
+        r = 0 -> 3, 6, 9, 12,..... Vanya not wins
+        
+        r = 1 -> 4, 7, 10, 13,.....  Vanya wins -> because it can decrease the number by 1
+        r = 2 -> 5, 8, 11, 14,.....  Vanya wins -> because it can increase the number by 1
+        
+        r = 3 -> 6, 9, 12, 15,.......  Vanya not wins
+
+
+        'Vanya goes first'
+
+        r = 0 : 3, 6, 9, 12,.....
+
+        if number i.e given itself is divisible by 3 eg : 6
+
+        n = 6, in Vanya move she have to increase or decrease that number one in order to win, 
+            let say she decrease by 1 -> 5, buy in vova term he will again increase by 1 in order to neutralize, so vanya didn't win...
+            or if she increase same things happens...
+            (va vo) (va vo) (va vo) (va vo) -> in 10th turn vo wins...
+        * Note : if number is divisible by 3 before the vanya move vova wins'
+        * Else in other case, if we see & observe vova wins..
+
+        * 10 is just a number that is given to 
  */ 
 
 #include<bits/stdc++.h>
@@ -25,18 +56,11 @@ typedef pair<int,int> pi;
 void solve(){
     int n;
     cin >> n;
-    for(int i=0;i<=10;i++){
-        if(n+1 % 3 == 0){
-            n++;
-            cout << "First" << endl;
-            return;
-        }else if( n-1 % 3 == 0){
-            n--;
-            cout << "First" << endl;
-            return;
-        }
+    if(n % 3 == 0){
+        cout << "Second" << endl;
+    } else {
+        cout << "First" << endl;
     }
-    cout << "Second" << endl;
 }
 
 
