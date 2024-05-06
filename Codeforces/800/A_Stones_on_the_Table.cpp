f29b1425c8796e0dabc48ee7720e7b9c0268810a
Sahil Sharma <sahilwep@gmail.com>
2024-05-06 21:22:45 +0530

    Codeforces: A. Stones on the Table

---
+++ Codeforces/800/A_Stones_on_the_Table.cpp
@@ -0,0 +1,48 @@
+/*
+//  A. Stones on the Table
+
+
+
+
+ */ 
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a)
+
+
+
+void solve(){
+    int n;
+    string s;
+    cin >> n >> s;
+    int res = 0;
+    for(int i=0;i<n;i++){
+        if(s[i] == s[i+1]){
+            res++;
+        }
+    }
+    cout << res << "\n";
+}
+
+
+int main(){
+    int t;     // Change the testcase according to question...
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
