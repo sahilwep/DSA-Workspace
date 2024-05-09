a263a1c492b185e427b8baf4d54555f350944bc2
Sahil Sharma <sahilwep@gmail.com>
2024-05-09 12:17:46 +0530

    codeforces:A. Wrong Subtraction

---
+++ Codeforces/800/A_Wrong_Subtraction.cpp
@@ -0,0 +1,48 @@
+/*
+//  A. Wrong Subtraction
+
+
+
+
+ */ 
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a)
+
+
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    for(int i=0;i<k;i++){
+        if(n % 10 != 0){
+            n--;
+        } else {
+            n /= 10;
+        }
+    }
+    cout << n << endl;
+}
+
+
+int main(){
+    int t;     // Change the testcase according to question...
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
