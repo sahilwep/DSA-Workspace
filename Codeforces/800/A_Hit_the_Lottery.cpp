59f19b740f9b23c41724cde164a7445a59548635
Sahil Sharma <sahilwep@gmail.com>
2024-05-20 17:53:56 +0530

    Codeforces: A. Hit the Lottery

--- Codeforces/800/A_Hit_the_Lottery.cpp
+++ Codeforces/800/A_Hit_the_Lottery.cpp
@@ -1,6 +1,5 @@
 /*
-//  
-
+//  A. Hit the Lottery
 
 
 
@@ -24,9 +23,28 @@ typedef pair<int,int> pi;
 
 
 void solve(){
-    int n;
-    
-    
+    ll n;
+    cin >> n;
+    int cnt = 0;
+    while(n != 0){
+        if(n - 100 >= 0){
+            n = n - 100;
+            cnt++;
+        } else if(n - 20 >= 0){
+            n = n - 20;
+            cnt++;
+        } else if(n - 10 >= 0){
+            n = n - 10;
+            cnt++;
+        } else if(n - 5 >= 0){
+            n = n - 5;
+            cnt++;
+        } else if(n - 1 >= 0){
+            n = n - 1;
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
 }
 
 
