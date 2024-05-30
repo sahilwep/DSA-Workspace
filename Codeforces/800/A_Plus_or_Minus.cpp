cc850ee001bf1ded0651dd586991fdf860c0b89d
Sahil Sharma <sahilwep@gmail.com>
2024-05-30 16:05:12 +0530

    codeforces: A. Plus or Minus

---
+++ Codeforces/800/A_Plus_or_Minus.cpp
@@ -0,0 +1,51 @@
+/*
+//  A. Plus or Minus
+
+
+// Observation: 
+    * a + b = c
+    * a - b = c
+
+we have to print + or -, which operation satisfied condition...
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+
+
+void solve(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a + b == c){
+        cout << "+" << endl;
+    }else{
+        cout << "-"  << endl;
+    }
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
