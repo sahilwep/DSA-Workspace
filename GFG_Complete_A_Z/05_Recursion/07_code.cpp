52b9b288cd9dada4426796912cea2336615b3080
Sahil Sharma <sahilwep@gmail.com>
2024-06-10 22:47:07 +0530

    Recursion: Writing base case in Recursion

--- GFG_Complete_A_Z/05_Recursion/07_code.cpp
+++ GFG_Complete_A_Z/05_Recursion/07_code.cpp
@@ -1,8 +1,8 @@
 /*
 // Writing base case in Recursion:
-
-
-
+    * Whenever we get some problem & we come up with subpart of solution, 
+    * but writing wrong base case will leads recursive call to stackOverFlow.
+        * So, we have to carefully choose the base case of recursive calls...
 
 
 */ 
@@ -11,16 +11,30 @@
 using namespace std;
 typedef long long ll;
 
+// factorial of number:
+int factorial(int n){
+    // if(n == 1) return 1;    // wrong base case, because for n = 0, segmentation fault..
+    // if(n == 0) return 1;    // right base case, or
+    if(n == 1 || n == 0) return 1;      // right base case
+    return n * factorial(n-1);
+}
 
+
+// nth fibonacci number:
+int fib(int n){
+    // base case: 
+    // if(n == 0) return 0;
+    // if(n == 1) return 1; 
+    if(n <= 1) return n;    // equivalent to upper statements..
+    return fib(n-1) + fib(n-2);
+}
 
 int main(void){
     
-    string s;
-    cin >> s;
-    for(int i=s.size();i>=0;i--){
-        
-    }
-    
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+    cout << fib(n) << endl;
     
     
     return 0;
