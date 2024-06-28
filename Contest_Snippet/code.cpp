f54daee94b63b29863279bbd49c22b68796502e9
Sahil Sharma <sahilwep@gmail.com>
2024-06-28 15:07:06 +0530

    Snippet Update

--- Contest_Snippet/code.cpp
+++ Contest_Snippet/code.cpp
@@ -11,7 +11,7 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> pi;
-typedef long unsigned int lsi;  // use during string traversal
+typedef size_t s_t;  // use during string traversal
 #define F first
 #define S second
 #define PB push_back
