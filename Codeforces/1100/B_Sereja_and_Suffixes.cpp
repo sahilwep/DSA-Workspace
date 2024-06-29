4a481cc4a5b812bd184071efe6a891e0a330c42a
Sahil Sharma <sahilwep@gmail.com>
2024-06-29 22:54:44 +0530

    Codeforces: B. Sereja and Suffixes

--- Codeforces/1100/B_Sereja_and_Suffixes.cpp
+++ Codeforces/1100/B_Sereja_and_Suffixes.cpp
@@ -2,7 +2,14 @@
 //  B. Sereja and Suffixes
 
 // Observations: 
-    * 
+    * For each li, we need to find how many distinct number are staying on the positions, li, li+1,....n
+    * Formally we need to find the distinct number among ali, ali+1,...an
+    * array elements are so large, we need to find each li.
+    * We are given li, i.e the position, from li-->n, we need to find the unique elements in that range.
+
+// Intrusion: 
+    * Bruteforce solution, will use nested loop approach, that will take O(n^2) complexity.
+        * By picking element one by one & checking if it's unique or not...
 
 
 */
@@ -22,12 +29,36 @@ typedef pair<int,int> pi;
 #define el cout << endl;
 #define SQ(a) (a)*(a);
 
+// Bruteforce Solution:
+int findUnique(int arr[],int n,int li){
+    int cnt = 0;
+    for(int i=li-1;i<n;i++){
+        bool flag = 0;
+        for(int j=i+1;j<n;j++){
+            if(arr[i] == arr[j]){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            cnt++;
+        }
+    }
+    return cnt;
+}
 
 
 void solve(){
-    int n;
-    
-    
+    int n, m;
+    cin >> n >> m;
+    int arr[n];
+    arrInp;
+    int l;
+    while(m--){
+        cin >> l;
+        cout << findUnique(arr, n, l) << endl;
+    }
+
 }
 
 
