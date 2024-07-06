75fa9ef2b6896aa5793740c8ff32189d2a0ae72f
Sahil Sharma <sahilwep@gmail.com>
2024-07-06 19:25:33 +0530

    Codeforces: A. Add Odd or Subtract Even

--- Codeforces/800/A_Add_Odd_or_Subtract_Even.cpp
+++ Codeforces/800/A_Add_Odd_or_Subtract_Even.cpp
@@ -8,6 +8,11 @@
         * choose any positive even integer y(y > 0) and replace a with a-y
     * You can perform as many such operations as we want...
     * we can choose same number x & y in different move...
+    * We need to find the minimum number of operations required to obtain b from a
+
+// Key observations: 
+    * We can increment numbers with odd value..
+    * And we can decrement it with even value..
 
 
 */
@@ -28,8 +33,6 @@ typedef size_t s_t;  // use during string traversal
 #define el cout << endl;
 #define SQ(a) (a)*(a);
 
-
-
 void solve(){
     int a, b;
     cin >> a >> b;
@@ -37,21 +40,28 @@ void solve(){
         cout << 0 << endl;
         return;
     }
-    int move = 1;
-    // when we have to decrement the value...
+    // when we have to increment the value, we can increment it with odd numbers..
+    if(b > a){  // when b is greater, means for a we can add only odd digits..
+        int req = b - a;
+        if(req % 2 != 0){ // when we get the required number as odd, only one number is needed..
+            cout << 1 << endl;
+        }else{  // else we need two numbers to make this possible
+            cout << 2 << endl;
+        }
+        return;
+    }
 
-    // when we have to increment the value...
-    
-    if(a > b && (a-b)%2 != 0){
-        move++;
+    // when we have to decrement the value, We can decrement it with even numbers..
+    if(b < a){  // when b is lesser, means for a we can decrement only even numbers... 
+        int req = a - b;
+        if(req % 2 == 0){   // when we get the required number as even, only one numbers needed..
+            cout << 1 << endl;
+        }else{  // else we need two numbers to make this possible..
+            cout << 2 << endl;
+        }
+        return;
     }
-    else if(b > a && (b % 2)%2==0){
-        move++;
-    }
-    cout << move << endl;
-    
 }
-
 
 int main(){
     int t = 1;     // Change the testcase according to question...
