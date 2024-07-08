6db68417a3c24242b115b921790edb27b6a47977
Sahil Sharma <sahilwep@gmail.com>
2024-07-08 09:23:24 +0530

    Codeforces: A. Array Divisibility

---
+++ Codeforces/800/A_Array_Divisibility.cpp
@@ -0,0 +1,50 @@
+/*
+//  A. Array Divisibility
+
+
+// Observations: 
+    * The example that we are given just to confuse us...
+    * If we just print the sequencial order numbers we the answer will be accepted...
+
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+void solve(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        arr[i] = i+1;
+    }
+    for(auto i : arr) cout << i << " ";
+    el;
+ 
+}
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
