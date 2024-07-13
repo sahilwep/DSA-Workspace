29999d93e0945ffa024c0f453343daac8c071d6d
Sahil Sharma <sahilwep@gmail.com>
2024-07-13 11:22:30 +0530

    Codeforces: B. Collatz Conjecture

--- Codeforces/1000/B_Collatz_Conjecture.cpp
+++ Codeforces/1000/B_Collatz_Conjecture.cpp
@@ -3,38 +3,52 @@
 
 
 // Observations: 
-    * 
+    * we have to follow the following operations k times..
+        * increase x by 1
+        * while the number x is divisible by y, divide it by y...
+        * In simple word we have to perform these operations: 
+            
+            while(k--){
+                x += 1;
+                while(x % y == 0){
+                    x = x / y;
+                }
+            }
+        * But this solutions will lead us to TLE
+
 
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-typedef size_t s_t;  // use during string traversal
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
-#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
-#define el cout << endl;
-#define SQ(a) (a)*(a);
 
-
-
-void solve(){
-    int x, k, y;
-    cin >> x >> k >> y;
-    while(k--){
-        
+void solve() {
+    ll x, y, z;
+    cin >> x >> y >> z;
+    x++;
+    z--;
+    while(x != y){
+        if(x % y == 0){
+            while(x % y == 0){
+                x /= y;
+            }
+        }
+        if(z == 0){
+            cout << x << endl;
+            return;
+        }
+        ll temp = y - x % y;
+        if(temp <= z){
+            x = x + temp;
+            z = z - temp;
+        }else{
+            cout << x + z << endl;
+            return;
+        }
     }
-    
-    
+    cout << 1 + z % (y-1) << endl;
 }
-
 
 int main(){
     int t = 1;     // Change the testcase according to question...
