b4dc8c2c82306a03c339efcf583904fdc18b1b5b
Sahil Sharma <sahilwep@gmail.com>
2024-07-14 22:16:22 +0530

    Codeforces: A. My First Sorting Problem

---
+++ Codeforces/800/A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,49 @@
+/*
+//  A. My First Sorting Problem
+
+// Observations: 
+    * We just have to sort the value & print it...
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+
+
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    if(a < b){
+        cout << a << " " << b << endl;
+    } else {
+        cout << b << " " << a << endl;
+    }
+    
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
