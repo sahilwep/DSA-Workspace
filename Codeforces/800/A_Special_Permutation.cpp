503d73b5b80c273ee99a02500ed1ae2d7ba07c06
Sahil Sharma <sahilwep@gmail.com>
2024-07-21 10:55:49 +0530

    Codeforces:  A. Special Permutation

---
+++ Codeforces/800/A_Special_Permutation.cpp
@@ -0,0 +1,52 @@
+/*
+//  A. Special Permutation
+
+// Observations: 
+    * We need to print an array which follows: (pi != i)
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+    int arr[n];
+    int k = n-1;
+    for(int i=n-1;i>=0;i--){
+        arr[i] = k--;
+    }
+    arr[0] = n;
+    for(auto i : arr){
+        cout << i << " ";
+    }
+    el;
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
