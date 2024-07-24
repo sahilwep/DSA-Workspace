e09279730cbbf3333b700b2068bfab0bb542de38
Sahil Sharma <sahilwep@gmail.com>
2024-07-24 22:39:24 +0530

    Codeforces: Array implementation of stack

--- GFG_Complete_A_Z/13_Stacks/02_code.cpp
+++ GFG_Complete_A_Z/13_Stacks/02_code.cpp
@@ -1,6 +1,10 @@
 /*
-// Array implementation of stack : 
+// Array implementation of stack
 
+
+// We have Primarily push(), pop(), peek(), & size() functions in stack..
+    * We will create an array at runtime, using new keyword..
+    * we will maintain top pointer that will point the top element of stack
 
 */
 
@@ -8,39 +12,37 @@
 using namespace std;
 
 struct MyStack{
-    int *arr;
-    int top;
-    int cap;
+    int *arr;   // create an array at runtime
+    int top;    // top will point the top of the value from stack.
+    int cap;    // this is the size of stack
+    // using constructor we have create an array of size c
     MyStack(int c){
-        cap = c;
-        top = -1;
-        arr = new int[c];
+        cap = c;    // staring capacity of stack
+        top = -1;   // initially stack is empty so it's point to index -1
+        arr = new int[c];   // creating an array at runtime of size c
     }
     void push(int x){
-        if(isFull()) return;
+        if(isFull()) return;    // if stack is full it will not push element into stack
         top++;
         arr[top] = x;
     }
     int pop(){
-        if(isEmpty()) return INT_MAX;
+        if(isEmpty()) return INT_MAX;   // if stack is empty it will not perform pop() operations
         int res = arr[top];
         top--;
         return res;
     }
     bool isEmpty(){
-        return (top == -1);
+        return (top == -1); // if top points -1, means there is no such element in stack..
     }
     bool isFull(){
-        return (top == cap-1);
+        return (top == cap-1);  // if top points to the last index means (cap-1) it will return true
     }
     int size(){
-        return top;
+        return (top + 1); // return the size of stack.
     }
     int peek(){
-        return arr[top];
-    }
-    void display(){
-        for(int i=0;i<cap;i++) cout << arr[i] << " ";
+        return arr[top];    // return the top element
     }
 };
 
@@ -53,7 +55,11 @@ int main(void){
     s.push(10);
     s.push(20);
 
-    s.display();
+    cout << "Size: " <<  s.size() << endl;
+    cout << "Top: " << s.peek() << endl;
+    s.pop();
+    cout << "Size: " <<  s.size() << endl;
+    cout << "Top: " << s.peek() << endl;
 
 
     return 0;
