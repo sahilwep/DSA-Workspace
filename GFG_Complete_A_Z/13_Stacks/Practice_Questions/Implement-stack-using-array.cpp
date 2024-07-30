cd540e923b48e290e461a564d1da5dc0c7bd27e6
Sahil Sharma <sahilwep@gmail.com>
2024-07-30 23:10:45 +0530

    Stack:  Implement stack using array

---
+++ GFG_Complete_A_Z/13_Stacks/Practice_Questions/Implement-stack-using-array.cpp
@@ -0,0 +1,63 @@
+/*
+
+// Implement stack using array: 
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+  private:
+    int arr[1000];
+    int top;
+
+  public:
+    MyStack() { top = -1; }
+    int pop();
+    void push(int);
+};
+
+// Push function
+void MyStack ::push(int x) {
+    if(top == 1000) return;
+    arr[++top] = x;
+}
+
+// Pop function
+int MyStack ::pop() {
+    if(top == -1) return -1;
+    return arr[top--];
+}
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore(); // Flush the newline character left by cin
+    while (T--) {
+        MyStack *sq = new MyStack();
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        int n = nums.size();
+        int i = 0;
+        while (i < n) {
+            int QueryType = nums[i++];
+            if (QueryType == 1) {
+                int a = nums[i++];
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+        delete sq;
+    }
+    return 0;
+}
