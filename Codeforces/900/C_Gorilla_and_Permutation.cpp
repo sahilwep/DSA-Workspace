aed92449c040681d3c229b7075c9e5b4322e81b4
Sahil Sharma <sahilwep@gmail.com>
2024-07-31 14:24:09 +0530

    Codeforces: A. Submission Bait

--- Codeforces/900/C_Gorilla_and_Permutation.cpp
+++ Codeforces/900/C_Gorilla_and_Permutation.cpp
@@ -2,9 +2,19 @@
 
 //  C. Gorilla and Permutation
 
+// Permutations: 
+    * Permutations means the selections of objects, where order of selections matters.
+    * nPr : n!/(n-r)!
+
 // Observations: 
     * gorilla & monk found three number n, m, & k, (m < k)
     * They decided to build permutation of length n 
+    * We have to build an array of elements that following the permutations conditions...
+        * a permutations of length n, &  (arr[i] <= n)
+    * if we carefully observe the input & output pattern:
+        * we first have to print the element that start from n to m...
+        * Then we have to print the element form 1 to m...
+
 
 
 */
@@ -26,10 +36,12 @@ typedef size_t s_t;  // use during string traversal
 #define SQ(a) (a)*(a);
 
 
-
 void solve(){
-    int n;
-    
+    int n, m, k;
+    cin >> n >> m >> k;
+    for(int i=n;i>m;i--) cout << i << " ";
+    for(int i=1;i<=m;i++) cout << i << " ";
+    el;
     
 }
 
