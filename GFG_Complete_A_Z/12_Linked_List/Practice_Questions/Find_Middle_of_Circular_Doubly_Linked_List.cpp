779bd6fb2f620ef6d920345a3993bf2239427780
Sahil Sharma <sahilwep@gmail.com>
2024-08-05 14:15:55 +0530

    Linked list: Find Middle of Circular Doubly Linked List

--- GFG_Complete_A_Z/12_Linked_List/Practice_Questions/Find_Middle_of_Circular_Doubly_Linked_List.cpp
+++ GFG_Complete_A_Z/12_Linked_List/Practice_Questions/Find_Middle_of_Circular_Doubly_Linked_List.cpp
@@ -3,6 +3,17 @@
 // Find Middle of Circular Doubly Linked List
     * We are given odd size circular linked list, we need to find middle element of it.
 
+// Observations: 
+    * We are given circular doubly inked list..
+    * We need to find the middle of circular linked list..
+    * The linked list size is odd...
+
+
+
+// Intrusion: 
+    * first we will iterate throughout the linked list & find the size of it..
+    * then we will iterate n/2 size of it, & return the middle value..
+
 
 */
 #include <bits/stdc++.h>
@@ -26,10 +37,23 @@ Node *newNode(int data){
 
 class Solution{
     public:
-    // 
+    // Function to find the middle of circular linked list:
     int findMiddle(Node * head){
+        Node *ref = head;   // making ref to compare at iteration time..
+        Node *curr = head->next;    // making curr to iterate 
+        int cnt = 1;    // as curr is decare head->next, so we declare count as 1
+        // iterate until our curr will not become head or ref we can say...
+        while(curr != ref){
+            cnt++;  // increment counter
+            curr = curr->next;  // updating current node..
+        }
 
+        // at last we will iterate till count/2, & return the value...
+        for(int i=0;i<cnt/2;i++){
+            ref = ref->next;
+        }
 
+        return ref->data;
     }
 };
 
