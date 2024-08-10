fd2ee29dd57e7b0f2de5a0e5c8fbdd83a22ee10e
Sahil Sharma <sahilwep@gmail.com>
2024-08-10 20:03:30 +0530

    Codeforces: A. EhAb AnD gCd

---
+++ Codeforces/800/A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,52 @@
+/*
+//  A. EhAb AnD gCd
+
+
+// Observations: 
+    * if n is divisible by 2, both the values are n/2
+    * else we have 1 & n-1 values..
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+    if(n % 2 == 0){
+        cout << n/2 << " " << n/2 << endl;
+    }
+    else{
+        cout << "1 " << n-1 << endl;
+    }
+    
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
