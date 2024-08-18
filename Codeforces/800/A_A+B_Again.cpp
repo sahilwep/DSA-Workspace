4d45e91ab498d5bd5a5b6b88538584be3a437f9b
Sahil Sharma <sahilwep@gmail.com>
2024-08-18 21:37:48 +0530

    Codeforces:   A. A+B Again?

---
+++ Codeforces/800/A_A+B_Again.cpp
@@ -0,0 +1,46 @@
+/*
+
+//  A. A+B Again?
+
+// Observations: 
+    * We are given integer of two digits, we need to find the sum of both the digits..
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+
+
+void solve(){
+    string s;
+    cin >> s;
+    int res = int(s[0] - '0') + int(s[1] - '0');
+    cout << res << endl;
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
