afeee214ce006fa0bab0e0f4f82a0fdd6125dd36
Sahil Sharma <sahilwep@gmail.com>
2024-08-20 00:12:56 +0530

    String:  Maximum Occuring Character

---
+++ GFG_Complete_A_Z/11_Strings/Practice_Questions/Maximum_Occuring_Character.cpp
@@ -0,0 +1,51 @@
+/*
+
+// Maximum Occuring Character
+
+// Observations: 
+    * We are given string s, we need to find the maximum occurring character in string.
+    * We have to print the maximum occurring lexigraphically smallest character.
+
+// Intrusion:
+    * We can use map, which sore characters in sorted manner..
+    * The we can return the maxFrequency element...
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    char getMaxOccuringChar(string s){
+        map<char, int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+
+        char maxChar;
+        int max = INT_MIN;
+        for(auto i=mp.begin();i!=mp.end();i++){
+            cout << i->first << " " << i->second << endl;
+            if(i->second > max){
+                max = i->second;
+                maxChar = i->first;
+            }
+        }
+
+        return maxChar;
+    }
+
+};
+
+int main(){
+   
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+    	Solution obj;
+        cout<< obj.getMaxOccuringChar(str)<<endl;
+    }
+}
