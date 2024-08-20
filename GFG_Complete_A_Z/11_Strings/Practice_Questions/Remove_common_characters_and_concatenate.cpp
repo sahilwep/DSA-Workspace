44bc2a74f05eea99af1d79d98b25b33afa2e7494
Sahil Sharma <sahilwep@gmail.com>
2024-08-20 12:27:19 +0530

    String: Remove common characters and concatenate

--- GFG_Complete_A_Z/11_Strings/Practice_Questions/Remove_common_characters_and_concatenate.cpp
+++ GFG_Complete_A_Z/11_Strings/Practice_Questions/Remove_common_characters_and_concatenate.cpp
@@ -13,17 +13,70 @@
         * TC: O(s1.size() * s2.size())
 
     // Efficient Approach: 
+        * we can use hashing approach.
+        * We can extract data which are unique, & then sort them & perform the searching operations..
+        * TC: O(max(s1.size(),s2.size()) * (log(max(s1, s2))))
+        
+                aacdb
+                gafd
+
 
 
 */
 
 #include <bits/stdc++.h> 
-using namespace std; 
+using namespace std;
 
 class Solution{
     public:
-    // Bruteforce Solution:
+    // Efficient Solution: 
+    // function to find the character from string.
+    bool searchString(string s, int start, int end, char x){
+        int mid = (start + end) / 2;
+        while(start <= end){
+            if(x == s[mid]){
+                return 1;
+            }else if(x > s[mid]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+            mid = (start + end) / 2;
+        }
+        return 0;
+    }
+
     string concatenatedString(string s1, string s2){
+        map<char, int> mp;
+        for(char c : s1) mp[c]++;
+        for(char c : s2) mp[c]++;
+
+        string res = "";
+        for(auto i=mp.begin();i!=mp.end();i++){
+            if(i->second == 1){
+                res += i->first;
+            }
+        }
+        string final = "";
+        sort(res.begin(), res.end());
+        for(int i=0;i<s1.size();i++){
+            bool found = searchString(res, 0, res.size(), s1[i]);
+            if(found){
+                final += s1[i];
+            }
+        }
+        for(int i=0;i<s2.size();i++){
+            bool found = searchString(res, 0, res.size(), s2[i]);
+            if(found){
+                final += s2[i];
+            }
+        }
+
+        return final;
+    }
+
+    // Bruteforce Solution:
+    string concatenatedString_(string s1, string s2){
         string res = "";
         // first getting the unique character from first string
         for(int i=0;i<s1.size();i++){
