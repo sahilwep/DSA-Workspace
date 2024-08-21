f22174e2995bf6e2cda0f7d2338542c308af4fdf
Sahil Sharma <sahilwep@gmail.com>
2024-08-21 17:49:36 +0530

    String: Non Repeating Character

--- GFG_Complete_A_Z/11_Strings/Practice_Questions/Non_Repeating_Character.cpp
+++ GFG_Complete_A_Z/11_Strings/Practice_Questions/Non_Repeating_Character.cpp
@@ -4,9 +4,20 @@
 
 // Observations: 
     * We are given string of size n, we have to return the first non-repeating character from stirng..
+    * If there is no repeating character, return "$"
 
 // Intrusion: 
-    * We can use hashing, approach: 
+    // BruteForce Solution: 
+        * we can use nested loop..
+        * TC: O(n*n)
+        * AS: O(1)
+
+    // Efficient Solution:
+        * We can use hashing, approach:
+        * Once we hash all the value into map, we can iterate throughout the string, & check for the first character that has frequency 1, if we are not able to find such character, we will return '$'
+        * TC: O(n)
+        * AS: O(n)
+ 
 
 */
 
@@ -15,9 +26,38 @@ using namespace std;
 
 class Solution{
 public:
-    char nonrepeatingCharacter(string S){
-      
-       
+    // Bruteforce Solution:
+    char nonrepeatingCharacter(string s){
+        for(int i=0;i<s.size();i++){
+            bool isFound = 0;
+            for(int j=0;j<s.size();j++){
+                // we have to skip the comparing position, which we using to comparing the whole string.
+                if(i != j){
+                    if(s[i] == s[j]){
+                        isFound = 1;
+                        break;
+                    }
+                }
+            }
+            if(!isFound){
+                return s[i];
+            }
+        }
+        return '$';
+    }
+
+    // Efficient Solution: 
+    char nonrepeatingCharacter_(string s){
+        unordered_map<char, int> mp;
+        for(char i : s) mp[i]++;
+
+        // checking the first Non repeating string
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]] == 1){
+                return s[i];
+            }
+        }
+        return '$';
     }
 
 };
