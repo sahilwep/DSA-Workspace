497f98555541affb5cb3b821db540f3b929c053c
Sahil Sharma <sahilwep@gmail.com>
2024-09-02 18:04:52 +0530

    Array : 2 Sum Problem

--- Striver_DSA_A-to-Z/Step-3-Array/Medium/2-Sum_Problem.cpp
+++ Striver_DSA_A-to-Z/Step-3-Array/Medium/2-Sum_Problem.cpp
@@ -12,7 +12,14 @@
         * We will use nested loop approach, & try to find two index which sum up target.
         * TC: O(n^2)
     
-
+    // Efficient Approach: 
+        * We will use hashmap in this approach, 
+        * As we have to find two element that sum up the target, if we think in that way: 
+            * target - arr[i] -> (arr[j] we will look this in map)
+            * if that values exist, we will return the current index, i.e the index on which the current element is found using hashmap i.e map[target - arr[i]]
+            * else we will return {-1, -1}
+        * TC: O(n)
+        * AS: O(n)
 
 */
 #include <bits/stdc++.h>
@@ -20,7 +27,26 @@ using namespace std;
 
 class Solution {
 public:
+    // Using hashing:  O(n)
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp; // declaring map
+
+        for(int i=0;i<n;i++){
+            int need = target - nums[i];    // getting the needed value, by subtracting arr[i] it from the target. 
+            // if the needed value found in the map, we will return the indexes
+            if(mp.find(need) != mp.end()){
+                return {mp[need], i};   // returning the index.
+            }
+            // hashing the index & the value into the hashmap.
+            mp[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+
+    // Bruteforce Approach:  O(n^2)
+    vector<int> twoSum_Brute(vector<int>& nums, int target) {
         vector<int> v;
         int n = nums.size();
         for(int i=0;i<n;i++){
