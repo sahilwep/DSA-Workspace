73ccb8149614848735476a101509c99ff8db11bc
Sahil Sharma <sahilwep@gmail.com>
2024-09-05 11:51:26 +0530

    Hashing:  Hashing for pair - 1 -> Brute + Hashing

--- GFG_Complete_A_Z/10_Hashing/Practice-Questions/Hashing_for_pair_1.cpp
+++ GFG_Complete_A_Z/10_Hashing/Practice-Questions/Hashing_for_pair_1.cpp
@@ -12,14 +12,38 @@
         * we will use nested loop to find such pair.
         * TC: O(n^2)
 
+    // Hashing Approach: 
+        * If we carefully observe: we are given sum,
+        * As we have to find two element that sum up the target, if we think in that way:
+            * target - arr[i] -> (arr[j] we will look this in map)
+            * If target value exist, we will return 1, at end we will return 0
+            * arr[i] + arr[j] = sum
+        * TC: O(n)
+        * AS: O(n)
 
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Hashing Approach: 
+int sumExists(int arr[], int n, int sum){
+    unordered_map<int, int> mp;
+    for(int i=0;i<n;i++){
+        int need = sum - arr[i];    // The value that we are looking for
+        // If needed value is found in hashmap
+        if(mp.find(need) != mp.end()){
+            return 1;
+        }
+        // if need value is not found: insert value into hashmap:
+        mp[arr[i]] = i;
+    }
+
+    return 0;
+}
+
 // bruteforce approach:
-int sumExists(int arr[], int n, int sum) {
+int sumExists_Brute(int arr[], int n, int sum) {
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             if(arr[i] + arr[j] == sum){
