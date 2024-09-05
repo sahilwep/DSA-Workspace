196ece91e03b602b5740921682282ad9d0445d81
Sahil Sharma <sahilwep@gmail.com>
2024-09-05 13:41:26 +0530

    Codeforces: A. And Then There Were K

---
+++ Codeforces/800/A_And_Then_There_Were_K.cpp
@@ -0,0 +1,51 @@
+/*
+
+// A. And Then There Were K
+
+
+// Observations: 
+    * given a integer n, find the maximum value of integer k such that the following condition holds.
+
+            n & (n-1) & (n-2) & (n-3) &....(k) = 0
+
+    * Here & denotes bitwise AND operands
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+    int msb = log2(n);
+    cout << ((1<<msb) - 1) << endl;
+
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
