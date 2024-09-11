c6186104e84ffda6524479b7500aa08746c90914
Sahil Sharma <sahilwep@gmail.com>
2024-09-11 09:41:15 +0530

    Sorting: Closet 0s 1s and 2s

--- GFG_Complete_A_Z/08_Sorting/Practice_Questions/Closet_0s_1s_and_2s.cpp
+++ GFG_Complete_A_Z/08_Sorting/Practice_Questions/Closet_0s_1s_and_2s.cpp
@@ -23,8 +23,27 @@ using namespace std;
 class Solution{
 public:
     // Using Sorting: O(nlogn)
-    void segragate012(int arr[], int n){
+    void segragate012_Sorting(int arr[], int n){
         sort(arr, arr+n);
+    }
+    // Using Counter: O(n)
+    void segragate012(int arr[], int n){
+        int one = 0, two = 0, zero = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0) zero++;
+            else if(arr[i] == 1) one++;
+            else two++;
+        }
+        int k = 0;
+        for(int i=0;i<zero;i++){
+            arr[k++] = 0;
+        }
+        for(int i=0;i<one;i++){
+            arr[k++] = 1;
+        }
+        for(int i=0;i<two;i++){
+            arr[k++] = 2;
+        }
     }
 };
 
