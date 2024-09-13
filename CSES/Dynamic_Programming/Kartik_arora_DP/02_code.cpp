c7e3980fc5915d026797e6bf2af20b5246f93d12
Sahil Sharma <sahilwep@gmail.com>
2024-09-13 15:48:29 +0530

    DP-2

--- CSES/Dynamic_Programming/Kartik_arora_DP/02_code.cpp
+++ CSES/Dynamic_Programming/Kartik_arora_DP/02_code.cpp
@@ -22,11 +22,18 @@
 
 
 // Recurrence:
-
+    * There is a relations that relates a particular dp state to some other dp state and that relation is known is recurrence.
 
 // Top-Down & Bottom-Up:
-
-
+    * Top Down: 
+        * In top down we are breaking the big problem into smaller sub-problem, & we will keep breaking it until we found a smaller problem that we can solve it directly.
+        * Write a recursive codes, that all the parameter defining sub problem & also keep memoizing the result those we get, we keep storing the result in an array, so that we are not solving same subproblem again.
+        * We start with final solution, & recursively break it into smaller subproblem, To avoid redundant calculations we store the result of solved subproblem into the memoization table.
+    
+    * Bottom Up: 
+        * First we are solving easier problem, & use their solutions to go upwards.
+        * First we will solve the most easiest problem & then use these result to solve hardest problem, & use their result to solve even hardest problem, until we solve the whole problem.
+        * Also known as tabulations, we start with smallest subproblem & gradually build up the final solution. we store the result into a table to avoid redundant calculations.
 
 
 */
