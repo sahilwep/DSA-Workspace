efb4ec345e18f741deeb6354b5a0f64c2a079de9
Sahil Sharma <sahilwep@gmail.com>
2024-09-13 11:44:20 +0530

    Searching: Floor in a Sorted Array -> Efficient method using lower_bond() function : O(logn) Time complexity

--- GFG_Complete_A_Z/07_Searching/Practice_Questions/Floor_in_a_Sorted_Array.cpp
+++ GFG_Complete_A_Z/07_Searching/Practice_Questions/Floor_in_a_Sorted_Array.cpp
@@ -26,11 +26,35 @@
             * If element is not found throughout the array, return last index.
         * TC: O(n)
 
+    // Efficient Solution: 
+        * We will use binary search, as array is sorted,
+        * There is some function on STL: lower_bond(), & upper_bond(), we can take help of them.
+        * lower_bond(): return the iterator pointing to the first element in the range[first, last] which has value not less than val.
+        * Example: 
+                Input: 10 20 30 40 50
+                Output: lower_bound for element 30 at index 2
+
+                Input: 10 20 30 40 50
+                Output: lower_bound for element 35 at index 3
+
+                Input: 10 20 30 40 50
+                Output: lower_bound for element 55 at index 5 (Basically, 55 is not present, so it returns end() iterator)
+
+                Input: 10 20 30 30 30 40 50
+                Output: lower_bound for element 30 at index 2
+
+
+        * NOTE: When we are using lower_bond() function we need to consider few things.
+                if(it != v.end() && *it == x) return *it - v.begin()    => same index of element that we found
+                else return *it - v.begin() -1  =>  return one index before if we are not able to find the element.
+
+        * TC: O(logn)
 
 
 */
 
 #include<bits/stdc++.h>
+#include<algorithm>
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
@@ -46,11 +70,21 @@ typedef size_t s_t;  // use during string traversal
 #define el cout << endl;
 #define SQ(a) (a)*(a);
 
-
 class Solution {
 public:
-    // Bruteforce Solution: 
+    // Efficient Solution:  
     int findFloor(vector<long long> &v, long long n, long long x) {
+        vector<long long>::iterator it = lower_bound(v.begin(), v.end(), x);
+        // when element is found then return the exact index of x.
+        if(it != v.end() && *it == x){
+            return it-v.begin();
+        }
+        else{
+            return it - v.begin() - 1;  // x is not found, so take previous element index as floor value.s
+        }
+    }
+    // Bruteforce Solution: 
+    int findFloor_Brutes(vector<long long> &v, long long n, long long x) {
         // Edge case: when element itself is smaller than the first element:
         if(x < v[0]){
             return -1;
