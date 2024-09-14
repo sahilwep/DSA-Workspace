9b164d2ac1ceccb5ed9d860047c671e2e9c3e589
Sahil Sharma <sahilwep@gmail.com>
2024-09-14 13:08:10 +0530

    Searching: Count 1's in binary array -> Brute O(n)

---
+++ GFG_Complete_A_Z/07_Searching/Practice_Questions/Count_1s_in_binary_array.cpp
@@ -0,0 +1,47 @@
+/*
+//  Count 1's in binary array
+
+
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+
+class Solution{
+public:
+    int countOnes(int arr[], int n){
+        
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        int *arr = new int[n]; 
+        for(int i = 0; i < n; i++)
+            cin>>arr[i];
+            
+        Solution ob;
+        cout <<ob.countOnes(arr, n)<<endl;
+    }
+    return 0;
+}
