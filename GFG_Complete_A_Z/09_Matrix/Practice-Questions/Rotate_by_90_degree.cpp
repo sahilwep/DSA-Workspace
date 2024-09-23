720707f80a76ff1436460e02452e193264e6c0b8
Sahil Sharma <sahilwep@gmail.com>
2024-09-23 19:32:21 +0530

    Matrix: Rotate by 90 degree -> With Using Auxilary Array, Without Using Auxilary Array

--- GFG_Complete_A_Z/09_Matrix/Practice-Questions/Rotate_by_90_degree.cpp
+++ GFG_Complete_A_Z/09_Matrix/Practice-Questions/Rotate_by_90_degree.cpp
@@ -42,9 +42,50 @@
         * TC: O(n*n)
         * AS: O(n*n)
     
-    
+
+    // Rotate without using auxilary array.
+        1 2 3       =>    3 6 9
+        4 5 6       =>    2 5 8
+        7 8 9       =>    1 4 7
+
+        Reverse row wise & column wise
 
 
+        // Example:
+                Matrix: 1  2  3  4 
+                        5  6  7  8 
+                        9  10 11 12 
+                        13 14 15 16 
+
+                Output: 4  8 12 16 
+                        3  7 11 15 
+                        2  6 10 14 
+                        1  5  9 13
+
+                // Step to perform:
+
+                    * First reverse every row:
+
+                        4  3  2  1
+                        8  7  6  5
+                        12 11 10 9 
+                        16 15 14 13 
+
+                    * swap row with column => Transpose of matrix
+
+                        4  8  12 16
+                        3  7  11 15
+                        2  6  10 14
+                        1  5  9  13
+                    
+                    * NOTE: To move value from row to column wise: 
+                        * We can use this logic: 
+                        i = 0 -> n
+                            j = i + 1 -> n
+                                swap(arr[i][j], arr[j][i])
+
+        * TC: O(n)
+        * AS: O(1)
 
 */
 
@@ -54,8 +95,25 @@ using namespace std;
 
 class Solution{   
 public:
-    //Function to rotate matrix anticlockwise by 90 degrees.
+    // Without using Auxilary array:
     void rotateby90(vector<vector<int> >& matrix, int n){
+        // First rotate every rows of array:
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+
+        // Reverse row with column:
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+    }
+
+
+    // Using Auxilary Array:
+    void rotateby90_Aux(vector<vector<int> >& matrix, int n){
 
         vector<vector<int> > temp(n, vector<int> (n));  // Auxilary space to perform all the operations
 
