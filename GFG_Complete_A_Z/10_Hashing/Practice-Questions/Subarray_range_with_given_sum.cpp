39a42ef8dbe514a79937d902f155712d77c24946
Sahil Sharma <sahilwep@gmail.com>
2024-09-27 16:27:44 +0530

    Hashing: Subarray range with given sum -> Brute, Hashing

--- GFG_Complete_A_Z/10_Hashing/Practice-Questions/Subarray_range_with_given_sum.cpp
+++ GFG_Complete_A_Z/10_Hashing/Practice-Questions/Subarray_range_with_given_sum.cpp
@@ -3,7 +3,8 @@
 //  Subarray range with given sum
 
 //  Observations: 
-    * We are given an array of size n, & we are given target, determine the subarray that element whose sum up to the target.
+    * We are given an array of size n, & we are given target, 
+    * determine the subarray that element whose sum up to the target.
 
 
 // Example:
@@ -22,7 +23,29 @@
         * Select each element one by one & try to find the subarray size, & once we get the sum we will increment the sum.
         * TC: O(n^2)
 
+    
+    // Hashing Approach:
+        * IF same element occur again, that means we have '0's in b/w the subarray or the sum of subarray is 0...
+        * According to the same observations if the value is just incremented by the sum, we would say that we get the sum.
+        * Example:
+            arr[] = 10 2 -2 -20 10
+            tar = -10
 
+
+            Edge case: mp[0] = 1
+
+
+            prefix sum:     sum - tar       Found
+               0,  1 ---------------BASE---------
+               10, 1        (10 - 10)  =>    x
+               12, 1        (12 - 10)  =>    x
+               10, 2        (10 - 10)  =>    Y
+               -10, 1       (10 - 10)  =>    Y
+               0, 2         (0 - 10)   =>    Y
+
+
+        * TC: O(n)
+        * AS: O(n)
 
 
 
@@ -30,6 +53,7 @@
 
 #include<bits/stdc++.h>
 #include<algorithm>
+#include<unordered_set>
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
@@ -48,8 +72,29 @@ typedef size_t s_t;  // use during string traversal
 
 class Solution {
 public:
-    // Bruteforce Approach: O(n^2)
+    // Hashing Approach: 
     int subArraySum(vector<int>& arr, int tar) {
+        int sum = 0;
+        int cnt = 0;
+        unordered_map<int, int> mp;
+        
+        // Handel the case where a subarray starting from beginning matches the target:
+        mp[0] = 1;
+
+        for(int i=0;i<arr.size();i++){
+            sum += arr[i];
+            if(mp.find(sum - tar) != mp.end()){
+                // cout << sum << " " << sum - tar << " " << mp[sum - tar] << endl;
+                cnt += mp[sum - tar];
+            }
+            mp[sum]++;
+        }
+
+        return cnt;
+    }   
+
+    // Bruteforce Approach: O(n^2)
+    int subArraySum_Brute(vector<int>& arr, int tar) {
         int cnt = 0;
 
         for(int i=0;i<arr.size();i++){
