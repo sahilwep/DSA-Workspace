9f2f096497ba5ba5514c9a180f9431890eb865a1
Sahil Sharma <sahilwep@gmail.com>
2024-10-09 01:31:03 +0530

    Leetcode 75: 394. Decode String

--- CSES/Dynamic_Programming/DP_Playlist/394.Decode_String.cpp
+++ CSES/Dynamic_Programming/DP_Playlist/394.Decode_String.cpp
@@ -37,7 +37,62 @@ The test cases are generated so that the length of the output will never exceed 
 
 
 //  Intrusion:
-    * 
+    // Bruteforce Approach: 
+        * we will keep iterating in string until we not find innermost bracket, whenever we get closing bracket,
+        * We will make our string that much times long..
+        * But this approach will take so much time..
+        * Example: 
+                2[a3[c2[x]]y]
+            * First work with the innermost bracket.
+                2[a3[cxx]y]
+            * then gradually go out of the zone.
+                2[acxxcxxcxxy]
+                acxxcxxcxxyacxxcxxcxxy  -> Answer
+        
+
+    // Efficient Approach: Using stack
+        * Two stacks will be used:
+            * numStack: To store the repeat count (integer k).
+            * strStack: To store the partially constructed string.
+        * Iterate through the input string:
+            * If a digit is encountered, calculate the full number (since it could be multiple digits).
+            * If an opening bracket [ is encountered, push the current repeat count and the current string onto their respective stacks, and reset the current string.
+            * If a closing bracket ] is encountered, pop the string from strStack and the number from numStack. Repeat the current string k times and append it to the string popped from strStack.
+            * If a letter is encountered, keep appending it to the current string.
+        * Output the final constructed string.
+
+        * Example: 2[a3[c2[x]]y]
+
+                        |       |           |   x   |
+                        |       |           |   [   |
+                        |       |           |   c   |
+                        |   2   |           |   [   |
+                        |   3   |           |   a   |
+                        |   2   |           |   [   |
+                        ---------           ---------
+                        numbers             string
+
+                    * Encounter with ']'
+                    * remove characters from string stack until we hit with '[' -> x
+                    * remove 2 from stack
+                    * repeat x 2 times => xx
+                    * Push it on the stack:
+                    
+                        |       |           |       |
+                        |       |           |   xx  |
+                        |       |           |   c   |
+                        |       |           |   [   |
+                        |   3   |           |   a   |
+                        |   2   |           |   [   |
+                        ---------           ---------
+                        numbers             string
+
+                    * Same steps repeat until we are not at the end of string..
+
+                    * At the end we will have our all the decoded strings into stack, just transfer it into string, then reverse it & return.
+
+        * TC: O(n)
+        * AS: O(n)
 
 
 */
@@ -46,12 +101,49 @@ The test cases are generated so that the length of the output will never exceed 
 #include<algorithm>
 using namespace std;
 
-
 class Solution {
 public:
     string decodeString(string s) {
-        
+        stack<int> numStack;          // Stack to store repeat counts
+        stack<string> strStack;       // Stack to store intermediate strings
+        string currentStr = "";       // Current string being processed
+        int num = 0;                  // Current number being processed
 
+        for(char c: s){
+            // if it's digit, from the number(could be multiple digits)
+            if(isdigit(c)){
+                num = num * 10 + (c - '0');
+            }
+            // Push the current number and string to stack, then reset them
+            else if(c == '['){
+                numStack.push(num);
+                strStack.push(currentStr);
+                num = 0;    // reset the number
+                currentStr = "";    // reset the current string
+            }
+            // If we encounter with closing bracket we need to process the element before them & number of times..
+            else if(c == ']'){
+                // Pop the number and the string from stacks:
+                int repeatCnt = numStack.top();
+                numStack.pop();
+                string temp = strStack.top();
+                strStack.pop();
+
+                // Append the current string 'repeatCnt' times:
+                for(int i=0;i<repeatCnt;i++){
+                    temp += currentStr; // With current string, adding temp on it.
+                }
+
+                // set the current string to the newly formed string:
+                currentStr = temp;
+            }
+            // Otherwise, it's a character, append to the current string:
+            else{
+                currentStr += c;
+            }
+        }
+
+        return currentStr;        
     }
 };
 
