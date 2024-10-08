8b53f93e062c930c723d592a02e4f0c7429b1d3c
Sahil Sharma <sahilwep@gmail.com>
2024-10-09 01:31:58 +0530

    Leetcode 75: 394. Decode String

--- CSES/Dynamic_Programming/DP_Playlist/394.Decode_String.cpp
+++ CSES/Dynamic_Programming/DP_Playlist/394.Decode_String.cpp
@@ -2,17 +2,6 @@
 
 //  394. Decode String
 
-The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is
- being repeated exactly k times. Note that k is guaranteed to be a positive integer.
-
-You may assume that the input string is always valid; there are no extra white spaces, square 
-brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain 
-any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
-
-The test cases are generated so that the length of the output will never exceed 105.
-
-
-
 //  Observations: 
     * We are given an encoding string, return decoded string.
     * The encoding rules are:
