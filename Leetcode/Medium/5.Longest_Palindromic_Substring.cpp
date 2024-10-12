63e611b1e9996160fc2a87aa2b445597cb310035
Sahil Sharma <sahilwep@gmail.com>
2024-10-12 11:09:18 +0530

    Leetcode: 5. Longest Palindromic Substring

--- Leetcode/Medium/5.Longest_Palindromic_Substring.cpp
+++ Leetcode/Medium/5.Longest_Palindromic_Substring.cpp
@@ -18,10 +18,38 @@
 // Intrusion:
     // Bruteforce Approach:
         * Using nested loop build a subarray that will be following the condition for palindrome
-        * TC: O(n^2) -> TLE
+        * TC: O(n^3) -> TLE
     
     // Efficient Approach:
-        * 
+        * A string is palindrome if it's same from left to right & right to left.
+        * We need to find longest palindromic substring.
+        * for any given string palindromic substring can be odd or even..
+        * Example:
+            * odd length substring
+                 s = b a b a d
+                       --|--
+                
+                                b a b a d
+                                  --|--
+                        * Here for odd we can pick any i, & grow towards left & right -> this is how we can find the longest substring..
+                        * For any 'i' if (i+1) & (i-1) are same, then we will grow the size, else return the substring..
+            
+            * even length substring
+
+                 s = b a b b a d
+                       -------
+        
+                                b a b b a d
+                                  --|----
+                                    i
+                        * Problem here in even case that the (i & i + 1) are same, & (i-1 & i+2) is same..
+                        * So, we need to take care of that also..
+
+
+            * Concluding the point, we need to check for even & odd case both.
+        
+        * TC: O(n^2)
+
 
 
 
@@ -34,9 +62,38 @@ using namespace std;
 class Solution {
 public:
     // Efficient Approach:
-    bool longestPalindrome(string s){
+    // function that will check the string by every i & return the substring.
+    string expandFromCenter(string s, int left, int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;     // grow towards left
+            right++;    // grow towards right
+        }
 
+        // (left + 1), because it's increment in last value, & same with right also..
+        return s.substr(left + 1, right - left - 1);    // returning the substring part.
     }
+    string longestPalindrome(string s){
+        int n = s.size();
+        if(n <= 1) return s;
+
+        string res = s.substr(0, 1);    // default we have single element itself a palindrome.
+
+        for(int i=0;i<n-1;i++){
+            string odd = expandFromCenter(s, i, i);     // In odd case we will pass same pointer as left & right.
+            string even = expandFromCenter(s, i, i+1);  // In even case we will pass one pointer ahead to i
+
+            // comparing the size of returned odd & even substring with previous substring & storing the result.
+            if(odd.size() >  res.size()){
+                res = odd;
+            }
+            if(even.size() > res.size()){
+                res = even;
+            }
+        }
+
+        return res;
+    }
+    
     // Bruteforce Solutions: 
     // function to check the string whether palindrome or not?
     bool isPalindrome(string s){
