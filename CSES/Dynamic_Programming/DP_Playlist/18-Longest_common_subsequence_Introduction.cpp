32260a2debeeba56511249296ac61799fea34943
Sahil Sharma <sahilwep@gmail.com>
2024-10-18 18:25:45 +0530

    DP: Longest common subsequence Introduction

---
+++ CSES/Dynamic_Programming/DP_Playlist/18-Longest_common_subsequence_Introduction.cpp
@@ -0,0 +1,49 @@
+/*
+
+// Longest common subsequence Introduction:
+
+    // Subsequence: 
+        * A subsequence is a sequence that can be derived from another sequence by deleting some or no elements, 
+        * without changing the order of the remaining elements.
+        * The characters do not need to be contiguous but must appear in the same order.
+        * Example:
+            For the string "abcde", the subsequences include "ace", "bde", and "abcde", 
+            but not "aec" because it does not follow the original order.
+
+    // Substring:
+        * A substring is a contiguous sequence of characters within a string.
+        * All characters of the substring must appear consecutively in the original string.
+        * Example:
+            For the string "abcde", the substrings include "a", "abc", "cde", 
+            but not "ace" because the characters are not consecutive.
+
+
+    // Subsets:
+        * A subset refers to a set of elements that can be formed from the elements of another set.
+        * In the case of strings, a subset can refer to the set of individual characters of the string, regardless of their order or position.
+        * Example:
+            For the string "abc", subsets of characters include {}, {"a"}, {"b", "c"}, {"a", "b", "c"}, etc. 
+            The order does not matter, and characters are treated as individual elements.
+
+
+    * We will exploring various question based on this LCS Particular Types:
+
+
+// Question Variations:
+    1. Longest Common subsequence
+    2. Print LCS.
+    3. Shortest Common Super-sequence
+    4. Print SCS
+    5. Minimum number of insertion & deletions
+    6. Longest Repeating Subsequence
+    7. Length of Longest Subsequence of "a" which is a substring in 'b'
+    8. Subsequence Pattern matching
+    9. Count how many times "a" appears as a subsequence in "b".
+    10. Longest Palindromic Subsequence.
+    11. Longest Palindromic Substring.
+    12. Count of the Palindromic Substring.
+    13. Minimum number of deletion in a string to make it a Palindrome.
+    14. Minimum number of insertion in a string to make it a Palindrome.
+
+
+*/
