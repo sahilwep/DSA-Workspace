000db96aab8ee3f6c33ef56c2e2c81a9f44f73d7
Sahil Sharma <sahilwep@gmail.com>
2024-12-12 04:16:39 +0530

    Leetcode: 2779. Maximum Beauty of an Array After Applying Operation -> Brute + Efficient using Queue

--- Leetcode/Medium/2779_Maximum_Beauty_of_an_Array_After_Applying_Operation.cpp
+++ Leetcode/Medium/2779_Maximum_Beauty_of_an_Array_After_Applying_Operation.cpp
@@ -33,6 +33,7 @@
         The beauty of the array nums is 4 (whole array).
 
 
+
 // Bruteforce Approach:
     -> This question is type of range based question:
         -> first get the range values of every nums[i]
@@ -41,12 +42,108 @@
         -> This gives TLE
 
 
+// Efficient Solution:
+    -> Break this question into intervals
+    -> Sort them based on their starting point.
+    -> We only need to see, before ending the first interval (a, b), whether our second interval (c, d) start or not?
+        -> If (b < c), whether our second interval start before ending the first interval or not?
+        -> Example:
+                        Input: nums = [4,6,1,2], k = 2
+                        Intervals: {(2, 6), (4, 8), (-1, 3), (0, 4)}
+                        Sorted intervals:   {(-1, 3), (0, 4), (2, 6), (4, 6)}
+
+                        -> Check whether the second interval start before ending the first interval or not?
+                        -> Case:
+                            ---------------
+                            (-1, 3), (0, 4)
+                            ---------------
+                                 i    j
+
+                                if (j < i)  -> Yes
+                                result = 2
+
+                        
+                        -> Case:
+
+                            -----------------------
+                            (-1, 3), (0, 4), (2, 6)
+                            -----------------------
+                                 i            j
+                                
+                                if(j < i) -> yes
+                                result = 3;
+                        
+                        -> Case:
+
+                            -------------------------------
+                            (-1, 3), (0, 4), (2, 6), (4, 6)
+                            -------------------------------
+                                 i                    j
+
+                                if(j < i) -> No => move i pointer forward -> remove element from back
+
+                            -----------------------
+                             (0, 4), (2, 6), (4, 6)
+                            -----------------------
+                                 i            j
+                                
+                                if(j <= i) -> Yes
+                                result = 3
+
+                        -> At max we have 3 in result
+                        -> This looks something like queue data-structure..
+                        -> While j > i => remove element from queue
+                        -> At every iteration we are getting the size of queue
+                             
+                            queue:
+                                    --------------------
+                                 <- pop                 <- push
+                                    --------------------
+
+
+// Intrusion: 
+    -> Intervals type question, where we have some range given, where multiple values lies on that range, think of a solution based on:
+        -> "Break it into Intervals, & sort them based on starting point"
+
 
 */
 
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
+
+
+class Solution {
+public:
+    // Function to calculate the maximum beauty value based on given conditions
+    int maximumBeauty(vector<int>& nums, int k) {
+
+        // First get all the range values of every nums[i]
+        vector<pair<int, int>> range;
+        for(int i = 0; i < nums.size(); i++) {
+            range.push_back({nums[i] - k, nums[i] + k});
+        }
+
+        sort(begin(range), end(range)); // sort based on starting point
+        int maxElement = 0;
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i < range.size(); i++) {
+            q.push({range[i].first, range[i].second});  // Push the current range into the queue
+
+            // Remove ranges from the queue that do not overlap with the current range
+            while(!q.empty() && q.front().second < range[i].first) {
+                q.pop();
+            }
+
+            int size = q.size();
+            maxElement = max(maxElement, size); // Update the maximum number of overlapping ranges
+        }
+
+        return maxElement;
+    }
+};
+
 
 
 // BruteForce Solution:
@@ -62,11 +159,11 @@ public:
 
         // hash all possible range:
         unordered_map<int, int> mp;
-        for(int i=0;i<range.size();i++){
+        for(int i=0;i<range.size();i++){    // this iterate in nums[i] size range: O(n)
             int start = range[i].first;
             int end = range[i].second;
 
-            // hashing all possible ranges:
+            // hashing all possible ranges:     // this will again iterate in O(n) in worse case
             for(int j=start;j<=end;j++){
                 mp[j]++;
             }
