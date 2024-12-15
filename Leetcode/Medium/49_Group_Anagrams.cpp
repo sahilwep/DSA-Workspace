2ab601649b76da0d54ab24af53fcf3be2b6563a5
Sahil Sharma <sahilwep@gmail.com>
2024-12-15 21:46:46 +0530

    Leetcode: 49. Group Anagrams -> Bruteforce + Efficient

--- Leetcode/Medium/49_Group_Anagrams.cpp
+++ Leetcode/Medium/49_Group_Anagrams.cpp
@@ -26,11 +26,41 @@
 
 
 // Approach:
-    // BruteForce Solution:
-        -> get the string portion from vector, Sort them
-        -> Check the further part of vector, if it's matched by sorting, then all of them into result.
-        -> When that string is included into result, mark them with some symbol so that we can't include & check it again..
+    // Brute Force Solution:
+        -> Iterate through each string in the vector.
+        -> Sort the current string and compare it with the sorted forms of other strings.
+        -> If a match is found, group them together into a result vector.
+        -> Mark the processed strings to avoid duplicates.
+        -> This solution results in a Time Limit Exceeded (TLE) error for larger inputs due to high time complexity.
+    
 
+    // Efficient Approach:
+        -> Utilize a hash map (unordered_map) to improve time complexity.
+        -> Key in the map is the sorted version of the string, which acts as a unique identifier for anagram groups.
+        -> Value in the map is a list of strings that are anagrams of the key.
+        -> Steps:
+            1. Iterate through each string, sort it to generate a key.
+            2. Use the key to group strings in the hash map.
+            3. Finally, transfer all grouped anagrams from the map into the result vector.
+
+        -> Map Structure:
+                {
+                "aet": ["eat", "tea", "ate"],
+                "ant": ["tan", "nat"],
+                "abt": ["bat"]
+                }
+
+
+// Complexity Analysis:
+
+    -> Brute Force:
+        TC: O(n^2 * k log k)
+            -> n = size of vector & k = avg length of string.
+        SC: O(n * k)
+
+    -> Efficient Approach:
+        TC: O(n * k * log k)
+        SC: O(n * k)
 
 
 */
@@ -38,6 +68,31 @@
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
+
+
+// Efficient Approach:
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& s) {
+        int n = s.size();
+
+        // First store all the anagram into map:
+        unordered_map<string, vector<string>> mp;
+        for(int i=0;i<n;i++){
+            string key = s[i];              // get the key
+            sort(key.begin(), key.end());   // sort the key
+            mp[key].push_back(s[i]);        // store that string into that key
+        }
+
+        // Now store all value from map to result:
+        vector<vector<string>> res;
+        for(auto i: mp){
+            res.push_back(i.second);
+        }
+
+        return res;
+    }
+};
 
 
 // BruteForce Solution:
