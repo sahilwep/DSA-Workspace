4a24c825382bfd4c13409e202bfa64bd44c600d6
Sahil Sharma <sahilwep@gmail.com>
2025-03-14 01:14:32 +0530

    Leetcode: Leetcode: 3356. Zero Array Transformation II -> 3 Solutions: Bruteforce, Good, Better | Difference Array Technique | Range Update

--- Leetcode/Medium/3356_Zero_Array_Transformation_II.cpp
+++ Leetcode/Medium/3356_Zero_Array_Transformation_II.cpp
@@ -39,7 +39,7 @@
         
 
 // Observations: 
-    ->  Given an array of integer of length n
+    -> Given an array of integer of length n
     -> given queries[i] = {l[i], r[i], val[i]}
     -> Each queries[i] represents the following actions on nums:
             -> decrement the value at each index in the range {l[i] r[i]} in nums bt AT MOST val[i]
@@ -54,12 +54,165 @@
             -> TC: O(Q * n)
             -> SC: (1)
 
+    // Good Solution:
+        -> We need to perform range update.
+        -> With using Difference Array technique we can perfrom all query update in O(n) time.
+        -> Now we will iterate in every query & perform difference array technique to only that range times.
+        -> And once all the value become '0', we will return that number of query.
+        
+        // Complexity:
+            -> TC: O(Q * (Q + n))
+            -> SC: O(n)
+
+    
+    // Better Solution:
+        -> With Difference Array technique, we will use binary search.
+        -> We can use binary search, because in question they were given that "after processing first k queries in sequence.." which directly saying queries are in order..
+        -> we will query only those portions query with comparing the results of difference array technique & we can save our time to not query all the queries.
+
+        
+        // Complexity: 
+            -> TC: O(log(Q) * (Q + n))
+            -> SC: O(n)
 
 */
 
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
+
+// Better Solution:
+
+class Solution {
+private:
+    int n, Q;
+    bool checkWithDiffArrayTeq(vector<int>& nums, vector<vector<int>>& queries, int k) {
+        vector<int> diff(n, 0);
+
+        // Generate difference value from given query:
+        for(int i = 0; i <= k; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int x = queries[i][2];
+
+            // Array Difference Technique:
+            diff[l] += x;
+            if(r + 1 < n) {
+                diff[r + 1] -= x;
+            }
+        }
+
+        // Now Generate Commutative Sum from difference array:
+        int cumSum = 0;
+        for(int i = 0; i < n; i++) {
+            cumSum += diff[i];
+
+            diff[i] = cumSum;
+
+            // if nums[i] still positive after subtracting 'k' queries values: -> return false then, we can't form zero array.
+            if(nums[i] - diff[i] > 0) {
+                return false;
+            }
+        }
+
+        return true;    // else return true, means we can form '0' array.
+    }
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) { // TC: O(log(Q) * ( Q + n))
+        n = nums.size();
+        Q = queries.size();
+
+        // Case: if all the values of nums is already '0'
+        // lambda function which will helps us to compare the value:
+        auto lambda = [] (int x) {
+            return (x == 0);    // compare x with '0'
+        };
+
+        if(all_of(begin(nums), end(nums), lambda)) {  // STL CPP function to check nums have '0' or not with passing lambda
+            return 0;   // no queries required, because all the values of nums is already '0'
+        } 
+        
+        // Using Binary Search:
+        int l = 0;      // first index of query
+        int r = Q - 1;  // last index of query
+        int k = -1;     // kth value which we have to return:
+
+        while(l <= r) {     // TC: O(log(Q))
+            int mid = l + (r - l) / 2;
+
+            if(checkWithDiffArrayTeq(nums, queries, mid) == true) { // TC: O(Q + n)
+                k = mid + 1;    // possible answer
+                // Now decrease the size, by set last value as "mid - 1" & explore in left side
+                r = mid - 1;    
+            }else { // if result is false: means we need further more queries.
+                l = mid + 1;    // increase the range of mid
+            }
+        }
+
+        return k;
+    }
+};
+
+// Good Solution:
+class Solution {
+private:
+    int n, Q;
+    bool checkWithDiffArrayTeq(vector<int>& nums, vector<vector<int>>& queries, int k) {
+        vector<int> diff(n, 0);
+
+        // Generate difference value from given query:
+        for(int i = 0; i <= k; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int x = queries[i][2];
+
+            // Difference Array Technique:
+            diff[l] += x;
+            if(r + 1 < n) {
+                diff[r + 1] -= x;
+            }
+        }
+
+        // Now Generate Commutative Sum from Difference array:
+        int cumSum = 0;
+        for(int i = 0; i < n; i++) {
+            cumSum += diff[i];
+
+            diff[i] = cumSum;
+
+            // if nums[i] still positive after subtracting 'k' queries values: -> return false then, we can't form zero array.
+            if(nums[i] - diff[i] > 0) {
+                return false;
+            }
+        }
+
+        return true;    // else return true, means we can form '0' array.
+    }
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        n = nums.size();
+        Q = queries.size();
+
+        // Case: if all the values of nums is already '0'
+        // lambda function which will helps us to compare the value:
+        auto lambda = [] (int x) {
+            return (x == 0);    // compare x with '0'
+        };
+
+        if(all_of(begin(nums), end(nums), lambda)) {  // STL CPP function to check nums have '0' or not with passing lambda
+            return 0;   // no queries required, because all the values of nums is already '0'
+        } 
+
+
+        for(int i = 0; i < Q; i++) {
+            if(checkWithDiffArrayTeq(nums, queries, i) == true) {
+                return i + 1;    // return i + 1, because of 1-based indexing.
+            }
+        }
+
+        return -1;  // else return -1, because not possible.
+    }
+};
 
 // Bruteforce Solution:
 class Solution {
