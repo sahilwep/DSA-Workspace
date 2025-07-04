239e8ef8ac43ec72e18a8fb32f74d1327a5d797f
Sahil Sharma <sahilwep@gmail.com>
2025-07-04 09:51:08 +0530

    Leetcode: 347. Top K Frequent Elements  | Max-Heap | min-Heap | Sorting | hashing | Priority-queue | Brute => Better => Better-improved => Efficient |

--- Leetcode/Medium/347_Top_K_Frequent_Elements.cpp
+++ Leetcode/Medium/347_Top_K_Frequent_Elements.cpp
@@ -17,9 +17,11 @@
         Output: [1]
 
 
+
 // Observations: 
     -> First store the frequency of every element
     -> Then return top k frequent occurring element.
+
 
 // BruteForce: 
     -> Hash the given list
@@ -32,14 +34,40 @@
         -> SC: O(n)
 
 
-// Efficient: 
+// Better: 
+    -> Sort the given array in decreasing order.
+    -> Store the frequency in vector in <freq: element> order
+    -> sort the frequency in decreasing order.
+    -> store the first top k element from frequency list.
+
+    // Complexity: 
+        -> TC: O(n * log N)
+        -> SC: O(n)
+
+
+
+// Better 2: max-heap
     -> Hash the given array.
     -> Push all the <frequency: element> into max heap
     -> return the top k element from heap.
 
     // Complexity: 
-        -> TC: O(n * log K)
+        -> TC: O(n * log N)
         -> SC: O(n)
+
+
+// Efficient: 
+    -> Hash the given array.
+    -> Push all the <freq: element> into min heap
+    -> if we get over to top 'k' pop-all the element from heap, while heap size is not lesser or equal to 'k'
+        -> This way we can pop all the lesser frequency element
+        -> We will left with only k-most occurring element into heap.
+
+    
+    // Complexity: 
+        -> TC: O(n * logK)
+        -> SC: O(n)
+
 
 
 */
@@ -48,8 +76,43 @@
 #include<algorithm>
 using namespace std;
 
+// Efficient Using min-heap: TC: O(n * logK)
+class Solution {
+private: 
+    typedef pair<int, int> pr;
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        // Hash the values:
+        unordered_map<int, int> mp;
+        for(auto &num: nums) {  // TC: O(n)
+            mp[num]++;
+        }
 
-// Efficient Solution: TC: O(n * logN)
+        // Store all the element in min-heap
+        priority_queue<pr, vector<pr>, greater<>> pq;
+        for(auto &[element, freq]: mp) {    // TC: O(n * log K)
+            pq.push({freq, element});
+            while(pq.size() > k) {  // only store top k-elements.
+                pq.pop();
+            }
+        }
+
+        // Return the top k-frequent element from heap.
+        vector<int> ans;
+        while(!pq.empty()) {    // TC: O(k)
+            auto [freq, element] = pq.top();
+            pq.pop();
+
+            ans.push_back(element);
+        }
+
+        return ans;
+    }
+};
+
+
+// Better with Heap: TC: O(n * logN)
 class Solution {
 private: 
     typedef pair<int, int> pr;
@@ -70,11 +133,53 @@ public:
 
         // Return the top k-frequent element from heap.
         vector<int> ans;
-        while(k--) {    // TC: O(n * log K)
+        while(k--) {    // TC: O(k)
             auto [freq, element] = pq.top();
             pq.pop();
 
             ans.push_back(element);
+        }
+
+        return ans;
+    }
+};
+
+
+// Better: Sorting => TC: O(n * log N)
+class Solution {
+private: 
+    typedef pair<int, int> pr;
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));   // sort the given array => TC: O(n * log N)
+
+        // Hash all the element into frequency vector:
+        vector<pair<int, int>> freq;
+        int cnt = 1;
+        for(int i = 1; i < n; i++) {    // TC: O(n)
+            if(nums[i - 1] == nums[i]) {
+                cnt++;
+            } else {
+                freq.push_back({cnt, nums[i-1]}); // store <freq: element>
+                cnt = 1;
+            }
+        }
+
+        freq.push_back({cnt, nums[n-1]});   // push last frequency count element.
+        
+
+        // sort the frequency vector in decreasing order:
+        sort(rbegin(freq), rend(freq)); // TC: O(n * log N)
+
+
+        // Build Result: 
+        vector<int> ans;
+        int i = 0;
+        while(k--) {    // TC: O(k)
+            auto [frequency, num] = freq[i++];
+            ans.push_back(num);
         }
 
         return ans;
