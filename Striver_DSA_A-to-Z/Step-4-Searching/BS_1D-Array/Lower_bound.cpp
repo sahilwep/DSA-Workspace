4cfe855e7fcd6a26c6f79d913377386b005fdf26
Sahil Sharma <sahilwep@gmail.com>
2025-08-14 19:05:28 +0530

    BS: Binary Search | Iterative & Recursive | lower_bound |

---
+++ Striver_DSA_A-to-Z/Step-4-Searching/BS_1D-Array/Lower_bound.cpp
@@ -0,0 +1,48 @@
+/*
+
+//  
+
+//  Problem Statement: 
+
+
+
+
+*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef size_t s_t;  // use during string traversal
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
+#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
+#define el cout << endl;
+#define SQ(a) (a)*(a);
+#define mod 1000000007
+
+
+
+void solve(){
+    int n;
+    
+    
+}
+
+
+int main(){
+    int t = 1;     // Change the testcase according to question...
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
