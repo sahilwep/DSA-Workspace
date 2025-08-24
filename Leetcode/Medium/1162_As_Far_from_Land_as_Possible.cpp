2e6470adf1df5afe1861eb2b7ca4d2c2abafad9d
Sahil Sharma <sahilwep@gmail.com>
2025-08-24 22:29:49 +0530

    Leetcode: 1162. As Far from Land as Possible | Multi-Source BFS | 2 Styles BFS |

--- Leetcode/Medium/1162_As_Far_from_Land_as_Possible.cpp
+++ Leetcode/Medium/1162_As_Far_from_Land_as_Possible.cpp
@@ -40,6 +40,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// BFS - level By level style processing:
+/*
+
+// Observations:
+    -> multi-source BFS
+    -> Return the max level it can reach.
+
+*/
+
+class Solution {
+private: 
+    int n, m;
+    int dir[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    bool isValid(int r, int c) {return (r >= 0 && r < n && c >= 0 && c < m);}
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        n = grid.size(), m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
+
+        // Initially push all the 1's into queue:
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) {
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        int level = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            bool isLvlFound = false;
+
+            while(size--) { // process every level one-by-one:
+                auto [row, col] = q.front();
+                q.pop();
+
+                for(int i = 0; i < 4; i++) {
+                    int r = row + dir[i][0];
+                    int c = col + dir[i][1];
+
+                    if(isValid(r, c) && grid[r][c] == 0 && !vis[r][c]) {
+                        vis[r][c] = 1;
+                        q.push({r, c});
+                        isLvlFound = true;
+                    }
+                }
+            }
+
+            if(isLvlFound) level++;
+        }
+
+        return (level == 0) ? -1 : level;   // if level is '0 return -1 : else return max lvl
+    }
+};
+
+
+// BFS Old Style: 
 class Solution {
 private:
     int n;
