0fd6c699b920bc68fe3a240bde906dcaa18cdec5
Sahil Sharma <sahilwep@gmail.com>
2025-10-23 14:34:04 +0530

    Leetcode:  3461. Check If Digits Are Equal in String After Operations I | String |

---
+++ Leetcode/Easy/3461_Check_If_Digits_Are_Equal_in_String_After_Operations_I.cpp
@@ -0,0 +1,85 @@
+/*
+
+//  3461. Check If Digits Are Equal in String After Operations I
+
+
+//  Problem Statement: 
+- You are given a string s consisting of digits. Perform the following operation repeatedly until the string has exactly two digits:
+    - For each pair of consecutive digits in s, starting from the first digit, calculate a new digit as the sum of the two digits modulo 10.
+    - Replace s with the sequence of newly calculated digits, maintaining the order in which they are computed.
+- Return true if the final two digits in s are the same; otherwise, return false.
+
+ 
+// Example: 
+    Example 1:
+    Input: s = "3902"
+    Output: true
+    Explanation:
+        Initially, s = "3902"
+        First operation:
+            (s[0] + s[1]) % 10 = (3 + 9) % 10 = 2
+            (s[1] + s[2]) % 10 = (9 + 0) % 10 = 9
+            (s[2] + s[3]) % 10 = (0 + 2) % 10 = 2
+            s becomes "292"
+        Second operation:
+            (s[0] + s[1]) % 10 = (2 + 9) % 10 = 1
+            (s[1] + s[2]) % 10 = (9 + 2) % 10 = 1
+            s becomes "11"
+        Since the digits in "11" are the same, the output is true.
+
+    Example 2:
+        Input: s = "34789"
+        Output: false
+        Explanation:
+
+            Initially, s = "34789".
+            After the first operation, s = "7157".
+            After the second operation, s = "862".
+            After the third operation, s = "48".
+            Since '4' != '8', the output is false.
+
+
+// Observations: 
+    - given string s consisting of digits
+    - perform operations repeatedly until the string has exactly two digits.
+        - For each pair of consecutive digits in s, starting from the first digit, calculate a new digit as the sum of the two digits modulo 10
+        - replace s with the sequence of newly calculate digit, maintaining the order in which they are computed.
+    - Return true if the final two digit in s are the same otherwise return false.
+
+    // Approach: 
+        - Implement the given operations: 
+        - perfrom sum operations in temp string, after that overwrite the original string.
+        - Do this until string.size() > 2
+        - lastly check the first & second index of string..
+
+    // Complexly: 
+        - TC: O(n^2)
+        - SC: O(1)
+
+*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+
+        while(s.size() > 2) {
+            string temp = "";
+            for(int i = 1; i < s.size(); i++) {
+                int first = (int)(s[i-1] - '0');
+                int second = (int)(s[i] - '0');
+                int sum = first + second;
+                string num = to_string(sum);
+                temp += (num.size() > 1) ? num[1] : num[0];
+            }
+
+            s = temp;   // overwrite new sum
+        }
+        
+        return (s[0] == s[1]);
+    }
+};
