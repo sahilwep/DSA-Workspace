09805b78939d47dab88b9131cd2d7f6f9370eebd
Sahil Sharma <sahilwep@gmail.com>
2026-01-03 16:49:57 +0530

    Leetcode: 64. Minimum Path Sum | DP | Recursion -> Memoization -> Tabulation | Graph | Dijkstra's Solution |

--- Striver_DSA_A-to-Z/Step-16-Dynamic_programming/03-2D_3D_DP_and_DP_on_GRID/Minimum_Path_Sum.cpp
+++ Striver_DSA_A-to-Z/Step-16-Dynamic_programming/03-2D_3D_DP_and_DP_on_GRID/Minimum_Path_Sum.cpp
@@ -2,13 +2,13 @@
 
 //  64. Minimum Path Sum
 
+
 //  Problem Statement: 
-    -> Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
-    -> Note: You can only move either down or right at any point in time.
+    - Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+    - Note: You can only move either down or right at any point in time.
 
  
 // Example: 
-
     Example 1:
         Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
         Output: 7
@@ -18,26 +18,7 @@
         Input: grid = [[1,2,3],[4,5,6]]
         Output: 12
 
-// Observations: 
-    -> We have to minimize the path sum-cost from (0, 0) to (n-1, m-1)
-    -> We can use Dijkstra's
-        - considerations factor is 'sum-cost'
-        - Src = (0, 0), Dst = (n - 1, m - 1)
-        - Dijkstra's process the shortest cost first.
-        - We can assume it's to be a 2D grid with weights
-        - Apply plain dijkstra..
-            - maintain 2D distance with initial value INT_MAX
-            - move across & Compute it with sum & move with smallest ones..
-
-        
-    // Complexity: 
-        -> TC: O((n * m) * (log(n * m)))
-        -> SC: O(n * m)
-
-
-// ---------------------- DP Solution ---------------------------
-
-
+    
 // Observations:
     - We have to start from (0, 0) & reach (n - 1, m - 1)
     - We can either travel right || OR || down
@@ -127,62 +108,19 @@
                 - SC: O(n * m)
 
 
+
+
+// Extra:
+    - This problem can also be solved using Dijkstra's
+    - Solution: LC: 64      ->      https://github.com/sahilwep/DSA-Workspace/blob/main/Leetcode/Medium/64_Minimum_Path_Sum.cpp
+
+
 */
 
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
 
-
-// Graph Solution:
-class Solution {
-private: 
-    typedef pair<int, pair<int, int>> ppr;
-    int n, m;
-    int dir[2][2] = {{0, 1}, {1, 0}};   // right, down
-    bool isValid(int r, int c) {return (r >= 0 && r < n && c >= 0 && c < m);}
-public:
-    int minPathSum(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-
-        vector<vector<int>> dist(n, vector<int> (m, INT_MAX));
-        priority_queue<ppr, vector<ppr>, greater<>> pq; // min-heap: this will keep smaller value at-top.
-
-        dist[0][0] = grid[0][0];
-        pq.push({grid[0][0], {0, 0}});
-
-        while(!pq.empty()) {
-            auto [cost, cor] = pq.top();
-            pq.pop();
-
-            int row = cor.first;
-            int col = cor.second;
-
-            if(row == n - 1 && col == m - 1) return cost;
-            if(cost > dist[row][col]) continue;
-
-            for(int i = 0; i < 2; i++) {
-                int r = row + dir[i][0];
-                int c = col + dir[i][1];
-                
-                if(isValid(r, c)) {
-                    int newCost = cost + grid[r][c];
-
-                    if(newCost < dist[r][c]) {
-                        dist[r][c] = newCost;
-                        pq.push({newCost, {r, c}});
-                    }
-                }
-            }
-        }
-        
-        return dist[n-1][m-1];
-    }
-};
-
-
-// -------------------------------- DP Solution ---------------------------------
 
 // Tabulations:
 class Solution {
@@ -269,5 +207,56 @@ public:
         m = grid[0].size();
 
         return solve(0, 0, grid);
+    }
+};
+
+
+// ---------------------- Graph Dijkstra's Solution ------------------------------
+
+
+// Graph Solution:
+class Solution {
+private: 
+    typedef pair<int, pair<int, int>> ppr;
+    int n, m;
+    int dir[2][2] = {{0, 1}, {1, 0}};   // right, down
+    bool isValid(int r, int c) {return (r >= 0 && r < n && c >= 0 && c < m);}
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+
+        vector<vector<int>> dist(n, vector<int> (m, INT_MAX));
+        priority_queue<ppr, vector<ppr>, greater<>> pq;
+
+        dist[0][0] = grid[0][0];
+        pq.push({grid[0][0], {0, 0}});
+
+        while(!pq.empty()) {
+            auto [cost, cor] = pq.top();
+            pq.pop();
+
+            int row = cor.first;
+            int col = cor.second;
+
+            if(row == n - 1 && col == m - 1) return cost;
+            if(cost > dist[row][col]) continue;
+
+            for(int i = 0; i < 2; i++) {
+                int r = row + dir[i][0];
+                int c = col + dir[i][1];
+                
+                if(isValid(r, c)) {
+                    int newCost = cost + grid[r][c];
+
+                    if(newCost < dist[r][c]) {
+                        dist[r][c] = newCost;
+                        pq.push({newCost, {r, c}});
+                    }
+                }
+            }
+        }
+        
+        return dist[n-1][m-1];
     }
 };
