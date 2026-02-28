a951535b00a73cddde6da234d01e39938b9e5259
Sahil Sharma <sahilwep@gmail.com>
2026-02-28 21:00:47 +0530

    Leetcode: 1404. Number of Steps to Reduce a Number in Binary Representation to One | Bit-magic | Binary Adding | Odd-even In Binary |

--- Leetcode/Medium/1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.cpp
+++ Leetcode/Medium/1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.cpp
@@ -71,37 +71,50 @@
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-typedef size_t s_t;  // use during string traversal
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-#define arrInp for(int i=0;i<n;i++) cin >> arr[i];
-#define arrOut(k) for(int i=0;i<n;i++) cout << arr[i] <<  k;
-#define el cout << endl;
-#define SQ(a) (a)*(a);
-#define mod 1000000007
 
 
+class Solution {
+private:
+    string addBinary(string& s1, string& s2) {
+        
+        int i = s1.size() - 1;
+        int j = s2.size() - 1;
+        int carry = 0;
+        string res = "";
 
-void solve(){
-    int n;
-    
-    
-}
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            
+            if(i >= 0 && s1[i] == '1') sum++;
+            if(j >= 0 && s2[j] == '1') sum++;
 
+            i--;
+            j--;
 
-int main(){
-    int t = 1;     // Change the testcase according to question...
-    cin >> t;
-    
-    while(t--){
-        solve();
+            res += (sum % 2) + '0';
+            carry = sum / 2;
+        }
+
+        reverse(begin(res), end(res));
+        
+        return res;
     }
-    
-    return 0;
-}
+public:
+    int numSteps(string s) {
+        int n = s.size();
+        
+        int cnt = 0;
+        string s2 = "1";
+        while(s != "1") {
+            if(s.back() == '1') {  // odd
+                s = addBinary(s, s2);   // get add '1' to it.
+            } else {    // even
+                s.pop_back();   // remove last bit, equivalent to right shift by 1 to divide by '2'
+            }
+
+            cnt++;
+        }
+
+        return cnt;
+    }
+};
