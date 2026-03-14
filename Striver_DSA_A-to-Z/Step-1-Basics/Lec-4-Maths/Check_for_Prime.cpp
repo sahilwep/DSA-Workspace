25632818be0a91ebdad0745cacc71c8333879bdc
Sahil Sharma <sahilwep@gmail.com>
2026-03-14 05:46:17 +0530

    Update Fix: Check Prime

--- Striver_DSA_A-to-Z/Step-1-Basics/Lec-4-Maths/Check_for_Prime.cpp
+++ Striver_DSA_A-to-Z/Step-1-Basics/Lec-4-Maths/Check_for_Prime.cpp
@@ -1,8 +1,35 @@
-//{ Driver Code Starts
+/*
+
+//  Check Prime:
+
+
+//  Problem Statement: 
+    - Given a number n, determine whether it is a prime number or not.
+    - Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+
+// Examples :
+    Input: n = 7
+    Output: true
+    Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
+
+    Input: n = 25
+    Output: false
+    Explanation: 25 has more than two divisors: 1, 5, and 25, so it is not a prime number.
+
+    Input: n = 1
+    Output: false
+    Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+
+    // Observations:
+        - For checking Prime number property for any number, we only need to iterate till the sqrt() of that number..
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-
+// Efficient Way:
 class Solution {
   public:
     bool isPrime(int n) {
@@ -19,18 +46,17 @@ class Solution {
     }
 };
 
+// Normal Way:
+class Solution {
+  public:
+    bool isPrime(int n) {
+        
+        if(n == 1) return false;    // 1 is not prime number as it has only single divisor
+        
+        for(int i = 2; i <= sqrt(n); i++) {
+            if(n % i == 0) return false;
+        }
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        Solution ob;
-        cout << (ob.isPrime(N) ? "true" : "false") << endl;
-
-        cout << "~"
-             << "\n";
+        return true;
     }
-    return 0;
-}
+};
